1eb150d8fd5225cf4587cca6ed4dc7d601499736
DougHennig_cp <SND\DougHennig_cp>
2008-11-04 16:28:15 +0000

    File added through Visual FoxPro.

---
+++ PropertyMethodDialogs/neweditpropertydialogenglish.h
@@ -0,0 +1,55 @@
+*==============================
+* Localization strings
+*==============================
+
+* Captions
+
+#define ccLOC_CAP_TITLE					'Edit Property/Method Dialog'
+
+#define ccLOC_CAP_NEW_PROPERTY			'New Property'
+#define ccLOC_CAP_NEW_METHOD			'New Method'
+#define ccLOC_CAP_NAME					'\<Name'
+#define ccLOC_CAP_VISIBILITY			'\<Visibility'
+#define ccLOC_CAP_ACCESS				'Acce\<ss Method' 
+#define ccLOC_CAP_ASSIGN				'Assign \<Method' 
+#define ccLOC_CAP_FAVORITES				'\<Favorites'
+#define ccLOC_CAP_DEFAULT				'Defau\<lt/Initial Value'
+#define ccLOC_CAP_DESCRIPTION			'\<Description'
+#define ccLOC_CAP_MEMBER_TYPE			'Type:'
+#define ccLOC_CAP_PROPERTY				'\<Property'
+#define ccLOC_CAP_METHOD				'M\<ethod'
+
+#define ccLOC_CAP_REMOVEPROMPT			'Remove <Insert1>?'
+
+#define ccLOC_CAP_PREFERENCES			'Preferences'
+#define ccLOC_CAP_APPLY					'Apply'
+#define ccLOC_CAP_REVERT				'Revert'
+#define ccLOC_CAP_REMOVE				'Remove'
+
+#define ccLOC_CAP_FILTERS				'Filters'
+#define ccLOC_CAP_PROPERTIES			'P\<roperties'
+#define ccLOC_CAP_METHODS				'Met\<hods'
+#define ccLOC_CAP_BOTH					'\<Both'
+#define ccLOC_CAP_NATIVE				'Na\<tive'
+#define ccLOC_CAP_NON_NATIVE			'N\<on-native'
+#define ccLOC_CAP_INHERITED				'\<Inherited'
+#define ccLOC_CAP_CUSTOM				'\<Custom'
+#define ccLOC_CAP_ALL					'\<All'
+
+#define ccLOC_CAP_ColHdrName			'Name'
+#define ccLOC_CAP_ColHdrType			'Type'
+#define ccLOC_CAP_ColHdrAccess			'Access'
+#define ccLOC_CAP_ColHdrAssign			'Assign'
+#define ccLOC_CAP_ColHdrVisibility		'Visibility'
+#define ccLOC_CAP_ColHdrHierarchy		'Hierarchy'
+#define ccLOC_CAP_ColHdrFavorite		'Favorite'
+
+
+* Messages
+
+#define ccLOC_STRING_TOO_LONG			"The member data for this class is over 8K in size, which is too long, so the display and favorites settings won't be saved."
+#define ccLOC_INVALID_XML				'The _MemberData property of this object contains invalid XML.'
+#define ccLOC_DIALOG_REGISTERED			'The Edit Property/Method dialog has been registered.'
+#define ccLOC_MEMBER_EXISTS				'<Insert1> already exists.'
+#define ccLOC_INVALID_CHAR				"That character isn't valid in a member name."
+#define ccLOC_NO_OBJECT					"There's no object to create a new property or method for."
