35b9f86831aba6d873a2dd5deb0279c4989bcbf5
DougHennig_cp <SND\DougHennig_cp>
2008-11-04 16:32:07 +0000

    File added through Visual FoxPro.

---
+++ PropertyMethodDialogs/newpropertydialogenglish.h
@@ -0,0 +1,36 @@
+*==============================
+* Localization strings
+*==============================
+
+* Captions
+
+#define ccLOC_CAP_NEW_PROPERTY			'New Property'
+#define ccLOC_CAP_NEW_METHOD			'New Method'
+#define ccLOC_CAP_NAME					'\<Name'
+#define ccLOC_CAP_VISIBILITY			'\<Visibility'
+#define ccLOC_CAP_ACCESS				'Acce\<ss Method' 
+#define ccLOC_CAP_ASSIGN				'Assign \<Method' 
+#define ccLOC_CAP_FAVORITES				'\<Favorites'
+#define ccLOC_CAP_DEFAULT				'Defaul\<t/Initial Value'
+#define ccLOC_CAP_DESCRIPTION			'\<Description'
+#define ccLOC_CAP_ADD					'Add'
+#define ccLOC_CAP_ADD_CLOSE				'Add & Close'
+#define ccLOC_CAP_CLOSE					'Close'
+#define ccLOC_CAP_MEMBER_TYPE			'Member type'
+#define ccLOC_CAP_PROPERTY				'\<Property'
+#define ccLOC_CAP_METHOD				'M\<ethod'
+#define ccLOC_CAP_USE_TOGGLE_EDITOR		'\<Use toggle editor'
+
+* Messages
+
+#define ccLOC_STRING_TOO_LONG			"The member data for this class is over 8K in size, which is too long, so the display and favorites settings won't be saved."
+#define ccLOC_INVALID_XML				'The _MemberData property of this object contains invalid XML.'
+#define ccLOC_DIALOG_REGISTERED			'New Property/Method dialog registered.'
+#define ccLOC_MEMBER_EXISTS				'<Insert1> already exists.'
+#define ccLOC_INVALID_CHAR				"That character isn't valid in a member name."
+#define ccLOC_NO_OBJECT					"There's no object to create a new property or method for."
+
+* Other strings
+
+#define ccLOC_MENU_NEW_PROPERTY			'New Property...'
+#define ccLOC_MENU_NEW_METHOD			'New Method...'
