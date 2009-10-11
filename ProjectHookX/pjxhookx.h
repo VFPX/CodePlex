42d3ada170b27be314859f5171d82b4514cf091f
tonifeltman_cp <SND\tonifeltman_cp>
2009-10-11 00:44:11 +0000

    Initial Check-in

---
+++ ProjectHookX/pjxhookx.h
@@ -0,0 +1,15 @@
+* Constants used by ProjectHookX
+
+#INCLUDE FoxPro.h
+
+#DEFINE ERROR_PROPERTY_IS_READONLY	1743
+#DEFINE ERROR_DATA_TYPE_MISMATCH	9
+#DEFINE ERROR_INVALID_SUBSCRIPT_REFERENCE	31
+
+#DEFINE NO_PJX_ACTIVE_LOC	"There is currently no project active, therefore the ProjectHookX cannot be instantiated."
+#DEFINE DUPLICATE_HOOK_CLASS_LOC	"Duplicate Hook Class. Cannot Add!"
+#DEFINE UNABLE_TO_SAVE_CHANGES_LOC	"Unable to Save Changes!"
+#DEFINE ERROR_NUMBER_LOC	"Error Number: "
+#DEFINE ERROR_MESSAGE_LOC	"Error Message: "
+#DEFINE FOR_LOC	" for "
+#DEFINE INVALID_CALL_TO_SELECTHOOKS	"Invalid parameters were passed to the Select Hooks Form. Cannot load form!"
